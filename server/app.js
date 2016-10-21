b156793fdebfa4efa71e960e3aa11494b4eb9ab7
Janitha Tennakoon <janitha000@gmail.comn>
2016-10-22 00:17:20 +0530

    Basic socket.io implemented

--- server/app.js
+++ server/app.js
@@ -1,74 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-
-app.use(function(req, res, next){
-  res.io = io;
-  next();
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+  console.log('A user connected');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+  //Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
   });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 http.listen(3001, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:3001');
 });
 
-
-module.exports = app;
