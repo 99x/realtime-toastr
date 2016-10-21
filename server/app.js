d8de1e751749b37dbbed801e78b4e881532b806e
Janitha Tennakoon <janitha000@gmail.comn>
2016-10-22 01:35:42 +0530

    Simple message from socket.io

--- server/app.js
+++ server/app.js
@@ -6,15 +6,22 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+//clients=[];
+
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
   console.log('A user connected');
+//  clients.push(socket);
 
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
     console.log('A user disconnected');
   });
+//setInterval(function() {
+//	clients[clients.length-1].emit('new message', "Last Connected User");
+//},1000);
 
+ socket.emit('new message', "toastr mesage");
 });
 
 http.listen(3001, function(){
