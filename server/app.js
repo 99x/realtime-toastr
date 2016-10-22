bed29bbf9939ba0d4bc8fa93b3ae6ffb51260dee
Janitha Tennakoon <janitha000@gmail.comn>
2016-10-22 05:44:25 +0530

    Added example clients

--- server/app.js
+++ server/app.js
@@ -6,23 +6,73 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-//clients=[];
+clients=[];
+types = ["Info", "Warn", "Success"];
 
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
   console.log('A user connected');
-//  clients.push(socket);
+  clients.push(socket.id);
 
   //Whenever someone disconnects this piece of code executed
   socket.on('disconnect', function () {
+      var i = clients.indexOf(socket.id);
+      clients.splice(i, 1);
+   
     console.log('A user disconnected');
   });
-//setInterval(function() {
-//	clients[clients.length-1].emit('new message', "Last Connected User");
-//},1000);
 
- socket.emit('new message', "toastr mesage");
+//setTimeout(function(){
+//	socket.emit('new message', socket.id);
+//},5000);
+
 });
+
+function sendErrorMessage(socketid, data){
+	io.to(socketid).emit('errorCount', data);
+}
+
+function sendFailMessage(socketid, data){
+	io.to(socketid).emit('FailMessage', data)
+}
+
+function sendSuccessMessage(socketid, data){
+	io.to(socketid).emit('SuccessMessage', data)
+}
+
+function createMessage(type, errorCount){
+	var jsonData = { "Type" : type, "Message" : "Error count " + errorCount};
+        return jsonData;
+}
+
+
+setInterval(function() {
+
+	var i = Math.floor(Math.random() * 3) + 0;
+        var length = clients.length; 
+        var client = Math.floor(Math.random() * length) + 0;  
+
+        console.log(i);
+	console.log(client);
+
+	switch(i) {
+	    case 0:
+		var errorCount = Math.floor((Math.random() * 100) + 1);
+        	var client = Math.floor(Math.random() * length) + 0;  
+        	var type = Math.floor(Math.random() * 3) + 0;
+        	var data = createMessage(types[type], errorCount);
+		sendErrorMessage(clients[client], data);
+		break;
+	    case 1:
+		sendFailMessage(clients[client], "THIS IS A FAIL");
+		break;
+	    case 2:
+		sendSuccessMessage(clients[client], "THIS IS A SUCCESS");
+	    default:
+		break;
+	}
+	
+},5000);
 
 http.listen(3001, function(){
   console.log('listening on *:3001');
